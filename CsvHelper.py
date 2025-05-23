02325febbea87eb28308a4b48ee6cfc3383753dc
Char Chen <charliecaresnot@gmail.com>
2025-05-23 18:48:00 +0800

    feat: sort tracks by their delta play count

--- CsvHelper.py
+++ CsvHelper.py
@@ -1,11 +1,25 @@
 
 import csv
 import os
+    
+def read_ids_and_played_counts_from_csv(file_path, id_col='ID', play_col='Played Count'):
+    play_count_dict = {}
+    try:
+        with open(file_path, 'r', encoding='mac_roman') as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                play_count_dict[row[id_col]] = int(row[play_col])
+    except Exception as e:
+        print(f"Error when reading file: {e}")
+        return {}
+    return play_count_dict
+
 
 def read_ids_from_csv(file_path='/Users/charliec/Library/CloudStorage/OneDrive-Personal/CharlieCares/charrrboard/RecentlyPlayed.csv'):
     with open(file_path, 'r', encoding='mac_roman') as file:
         tracks = [row[0] for row in csv.reader(file)][1:]  # 跳过表头
         return tracks
+
 
 def get_delta_played_counts(persistent_ids, csv_dir='/Users/charliec/Library/CloudStorage/OneDrive-Personal/CharlieCares/charrrboard'):
     delta_dict = {}
@@ -29,21 +43,17 @@ def get_delta_played_counts(persistent_ids, csv_dir='/Users/charliec/Library/Clo
             
     return delta_dict
 
-    
-def read_ids_and_played_counts_from_csv(file_path, id_col='ID', play_col='Played Count'):
-    play_count_dict = {}
-    try:
-        with open(file_path, 'r', encoding='mac_roman') as file:
-            csv_reader = csv.DictReader(file)
-            for row in csv_reader:
-                play_count_dict[row[id_col]] = int(row[play_col])
-    except Exception as e:
-        print(f"Error when reading file: {e}")
-        return {}
-    return play_count_dict
 
+def sort_tracks_by_delta_play_counts(delta_dict, reverse=True):
+    return [
+        item[0] for item in
+        sorted(delta_dict.items(), key=lambda x: x[1], reverse=reverse)
+    ]
+    
 
 # MAIN ACTIONS
 ids = read_ids_from_csv()
 result = get_delta_played_counts(ids)
 print(result)
+sorted = sort_tracks_by_delta_play_counts(result)
+print(sorted)
