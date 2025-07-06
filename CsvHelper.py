2f060c8a8f323ab4dd3d029ab7690d9a939dcede
Char Chen <charliecaresnot@gmail.com>
2025-07-06 03:02:00 +0200

    refactor: create playlist also from python

--- CsvHelper.py
+++ CsvHelper.py
@@ -12,7 +12,8 @@ from typing import Dict, List, Tuple
 WORKING_FOLDER_DIR = "/Users/charliec/myDev/AppleScripts/AppleMusicWeeklyChart/WorkingFolder"
 RECENTLY_PLAYED_PATH = WORKING_FOLDER_DIR + "/RecentlyPlayed.csv"
 ALL_EXPORT_DIR_PREFIX = WORKING_FOLDER_DIR + "/AllExport_"
-TRACK_INFO_GRABBER_AS_PATH = "/Users/charliec/myDev/AppleScripts/AppleMusicWeeklyChart/TrackInfoGrabber.scpt"
+TRACK_INFO_GRABBER_SCPT_PATH = "/Users/charliec/myDev/AppleScripts/AppleMusicWeeklyChart/TrackInfoGrabber.scpt"
+PLAYLIST_CREATOR_SCPT_PATH = "/Users/charliec/myDev/AppleScripts/AppleMusicWeeklyChart/PlaylistCreator.scpt"
 MIN_PLAYCOUNT = 3
 
 def read_ids_and_played_counts_from_csv_batches(batch_dir):
@@ -33,7 +34,7 @@ def read_ids_and_played_counts_from_csv_batches(batch_dir):
 def read_ids_and_played_counts_from_csv(file_path, id_col='ID', play_col='Played Count'):
     play_count_dict = {}
     try:
-        with open(file_path, 'r', encoding='mac_roman') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             csv_reader = csv.DictReader(file)
             for row in csv_reader:
                 play_count_dict[row[id_col]] = int(row[play_col])
@@ -101,9 +102,16 @@ def save_tracks_to_csv(sorted_data: List[Tuple[str, int]], output_path: str) -> 
     print('Weekly chart created at ' + output_path + '.')
 
 
+def read_pids_from_csv(csv_path: str) -> list[str]:
+    with open(csv_path, newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        pids = [row[0] for row in reader if row]  # avoid empty rows
+    return pids
+
+
 def get_track_info(pid: str):
     result = subprocess.run(
-        ["osascript", TRACK_INFO_GRABBER_AS_PATH, pid],
+        ["osascript", TRACK_INFO_GRABBER_SCPT_PATH, pid],
         capture_output=True,
         text=True
     )
@@ -115,12 +123,27 @@ def get_track_info(pid: str):
 
 
 def compare_play_count_records(new_date: str, old_date: str, output_path: str = None):
+    print(f"Comparing playcount data from {old_date} to {new_date}...")
     result = get_delta_played_counts(new_export_dir=ALL_EXPORT_DIR_PREFIX + new_date, old_export_dir=ALL_EXPORT_DIR_PREFIX + old_date)
     sorted = sort_tracks_by_delta_play_counts(result)
     if not output_path:
         output_path = WORKING_FOLDER_DIR + "/Chart_" + new_date + "_" + old_date + ".csv"
     save_tracks_to_csv(sorted, output_path)
-
+    return output_path
+    
+    
+def create_playlist_from_pids(pids: list[str], playlist_name: str):
+    pid_string = ",".join(pids)
+    result = subprocess.run(
+        ["osascript", PLAYLIST_CREATOR_SCPT_PATH, pid_string, playlist_name],
+        capture_output=True,
+        text=True
+    )
+    
+    if result.returncode == 0:
+        print("Success:", result.stdout.strip())
+    else:
+        print("Error:", result.stderr.strip())
 
 # def compare_play_count_records_by_day(delta_day):
 #     today = date.today().strftime("%Y.%m.%d")
@@ -147,9 +170,9 @@ def compare_play_count_records(new_date: str, old_date: str, output_path: str = 
 
 
 # MAIN ACTIONS
-# delta_day = 7
-# delta_month = 0
-# delta_year = 0
+delta_day = 7
+delta_month = 0
+delta_year = 0
 # if len(sys.argv) > 1:
 #     try:
 #         delta_day, delta_month, delta_day = parse_duration(sys.argv[1])
@@ -166,6 +189,7 @@ def compare_play_count_records(new_date: str, old_date: str, output_path: str = 
 # old_date = subtract_ymd(delta_year, delta_month, delta_day, from_date=new_date)
 # compare_play_count_records(old_date.strftime("%Y.%m.%d"), new_date.strftime("%Y.%m.%d"))
 
+##### MAIN ACTIONS #####
 new_date = date.today()
 if len(sys.argv) > 1:
     try:
@@ -173,11 +197,20 @@ if len(sys.argv) > 1:
     except ValueError:
         print(f"Invalid argument: '{sys.argv[1]}' cannot be parsed. Using today by default.")
 
-old_date = new_date - relativedelta(years=0, months=0, days=7)
+old_date = new_date - relativedelta(years=delta_year, months=delta_month, days=delta_day)
 if len(sys.argv) > 2:
     try:
         old_date = datetime.strptime(sys.argv[2], "%Y.%m.%d").date()
     except ValueError:
         print(f"Invalid argument: '{sys.argv[2]}' cannot be parsed. Using one week ago from the new date by default.")
 
-compare_play_count_records(new_date.strftime("%Y.%m.%d"), old_date.strftime("%Y.%m.%d"))
+csv_path = compare_play_count_records(new_date.strftime("%Y.%m.%d"), old_date.strftime("%Y.%m.%d"))
+chart_type = (
+    "周榜" if delta_day == 7 else
+    "月榜" if delta_month == 1 else
+    "年榜" if delta_year == 1 else
+    ""
+)
+
+pids = read_pids_from_csv(csv_path)
+create_playlist_from_pids(pids, f"Charrrboard {chart_type} {new_date.strftime('%Y.%m.%d')}")
